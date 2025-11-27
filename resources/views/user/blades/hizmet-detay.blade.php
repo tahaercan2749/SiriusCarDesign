6dca5789995f1aba0c370ba308292ad7263256de
Bülent ERCAN <tahaercan2749@gmail.com>
2025-11-27 14:43:36 +0300

    yayına hazır

--- resources/views/user/blades/hizmet-detay.blade.php
+++ resources/views/user/blades/hizmet-detay.blade.php
@@ -1,4 +1,40 @@
 @extends("user.partial.layout")
+@section("extraCss")
+    @php
+        // SSS değişkeni var mı ve dolu mu kontrolü
+        if (isset($page->faq) && count($page->faq) > 0) {
+
+            $questions = [];
+
+            foreach($page->faq as $soru) {
+                // Soruları ve cevapları listeye ekle
+                $questions[] = [
+                    "@type" => "Question",
+                    "name" => $soru->question,
+                    "acceptedAnswer" => [
+                        "@type" => "Answer",
+                        "text" => strip_tags($soru->answer) // HTML etiketlerini temizle
+                    ]
+                ];
+            }
+
+            // Ana FAQPage şemasını oluştur
+            $faqSchema = [
+                "@context" => "https://schema.org",
+                "@type" => "FAQPage",
+                "@id" => url()->current() . "#faq",
+                "mainEntity" => $questions
+            ];
+        }
+    @endphp
+
+    {{-- Eğer şema oluşturulduysa scripti bas --}}
+    @if(isset($faqSchema))
+        <script type="application/ld+json">
+            {!! json_encode($faqSchema, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT) !!}
+        </script>
+    @endif
+@endsection
 @section("content")
 
     <div class="inner-page">
@@ -11,6 +47,30 @@
         <div class="max-width  content-space with-sidemenu">
             <div class="content">
                 {!! $page->content !!}
+
+                <h2>Sıkça Sorulan Sorular</h2>
+                @if($page->faq)
+                    <div class="our-values content-space">
+                        <div class="values">
+                            @foreach($page->faq as $soru)
+                                <div class="value spec-bg-stroke @if($loop->index==0) active @endif">
+                                    <div class="title">
+                                        <h3>{{$soru->question}}</h3>
+                                        <figure>
+                                            <img src="{{asset("images/inner/arrow-up-icon.svg")}}" width="25"
+                                                 alt="">
+                                        </figure>
+                                    </div>
+
+                                    <div class="content">
+                                        <p>{{$soru->answer}}</p>
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
+                @endif
+
             </div>
             <div class="sidemenu">
                 @if($page)
@@ -21,7 +81,7 @@
                         @foreach($page->parentCategory->parent->children as $item)
                             @if($item->page)
                                 <li class="spec-stroke {{ $page->id == $item->page->id ? 'active' : '' }}">
-                                    <a href="{{$item->page->slug}}" >{{$item->page->title}}</a>
+                                    <a href="{{$item->page->slug}}">{{$item->page->title}}</a>
                                 </li>
                             @else
                                 <li class="spec-stroke deactive"><a>{{$item->name}}</a>
@@ -32,6 +92,8 @@
                     </ul>
                 @endif
             </div>
+
+
         </div>
     </div>
 
